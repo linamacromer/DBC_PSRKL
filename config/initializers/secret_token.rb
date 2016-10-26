45a8c5eef26f661dface5dc9b4ce7f46c84c6605
Lina Cromer <linamacromer@gmail.com>
2016-10-26 11:12:55 -0700

    Generate rails with 'rails new DBC_PSRKL --database=postgresql -T'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DBCPsrkl::Application.config.secret_key_base = '34a4f6d7624934f7630ae76c4537bd3f2b7bfea94a26511192f8a79eca1246be290ce773d993b2ac0572d3269df63b91862b57d7f3745215958230092fca6f26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DBCPsrkl::Application.config.session_store :cookie_store, key: '_DBC_PSRKL_session'
