3b1f89b4abc4526065ddcb7019c195ab3cb3306d
Alex Perez & Jason Bulicek <pair+ap2539.bulicekj@devbootcamp.com>
2016-10-26 12:44:07 -0700

    Define models for comptetitors and matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,13 @@
 class Match < ActiveRecord::Base
   belongs_to :competitor1, class_name: "Competitor"
   belongs_to :competitor2, class_name: "Competitor"
+  belongs_to :winner, class_name: "Competitor"
+  belongs_to :loser, class_name: "Competitor"
+
+  validates_presence_of :competitor1, :competitor2, :start_time, :season, :location
+
+  def competitors
+    [self.competitor1, self.competitor2]
+  end
 
 end
