6074f4da178e7d0a44fe74b416198c4e002b75a9
Paul Liu & Warner Nielsen <pair+paulliu87.warniel08@devbootcamp.com>
2016-10-27 14:43:29 -0700

    Month Filter

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,24 +1,23 @@
 class CalendarController < ApplicationController
   def index
-    @matches = Match.all
-  end
+    # @matches = Match.all
+    p params
+    if params[:start_date] != nil
+      @start_date = convert_to_date(params[:start_date])
+      @end_date = @start_date.end_of_month()
+      @matches = Match.where(:start_time => @start_date..@end_date)
+    else
+      @matches = Match.all
+    end
 
-  def create
-
-    p convert_to_date(params[:start_date])
-    # @article = Article.new(article_params)
-
-    # if @article.save
-    #   redirect_to @article
-    # else
-    #   render 'new'
-    # end
   end
 
   private
     def convert_to_date(string)
       if string == "January"
-
+        DateTime.new(2016,01)
+      elsif string == "October"
+        DateTime.new(2016,10)
       end
     end
 end
