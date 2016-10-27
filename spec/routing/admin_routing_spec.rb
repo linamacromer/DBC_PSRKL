03660cfa313ae756155ae46d85a7d31e44a6cc84
apprentice <apprentice@apprentice-6.local>
2016-10-27 10:41:18 -0700

    admin index route

--- spec/routing/admin_routing_spec.rb
+++ spec/routing/admin_routing_spec.rb
@@ -1,39 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Routing to admin", :type => :routing do
-  context 'when admin is logged in' do
-
-    # Fake pending test to remind us to return
-    it 'does the routing with the right authentication'
-    # When we figure out security we can make this more specific
-    before(:each) do
-      @request.session[:logged_in] = true
-    end
-
     it "routes GET /admin to admin#index" do
       expect(:get => "/admin").to route_to("admin#index")
     end
-
-    # When you do admin competitors routes, add the rest here
-    it "routes GET /admin/competitors to competitors#index" do
-      expect(:get => "/admin/competitors").to route_to("competitors#index")
-    end
-
-  end
-
-  context 'when admin is not logged in' do
-    before(:each) do
-      @request.session[:logged_in] = false
-    end
-
-    it "routes GET /admin to admin#login" do
-      expect(:get => "/admin").to route_to("admin#login")
-    end
-
-    # When you do admin competitors routes, add the rest here
-    it "routes GET /admin/competitors to admin#login" do
-      expect(:get => "/admin/competitors").to route_to("admin#login")
-    end
-
-  end
 end
