e9f636719b391c53b3b8dfd5d47f43dd1be640be
apprentice <apprentice@apprentice-6.local>
2016-10-27 15:25:05 -0700

    Add bikehorn

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(".audioButton").on("click", function() {
+  $(".audio-play")[0].currentTime = 0;
+  return $(".audio-play")[0].play();
+});
