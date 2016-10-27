4219b3f1cf7ede2b29e54e26faae3aa0b5181f5e
Paul Liu & Warner Nielsen <pair+paulliu87.warniel08@devbootcamp.com>
2016-10-27 15:22:05 -0700

    Add css style

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,23 +1,22 @@
 class CalendarController < ApplicationController
   def index
-    # @matches = Match.all
-    p params
     if params[:start_date] != nil
       @start_date = convert_to_date(params[:start_date])
       @end_date = @start_date.end_of_month()
-      @matches = Match.where(:start_time => @start_date..@end_date)
+      @matches = Match.where(:start_time => @start_date..@end_date).order(:start_time)
     else
-      @matches = Match.all
+      @matches = Match.all.order(:start_time)
     end
-
   end
 
   private
     def convert_to_date(string)
-      if string == "January"
-        DateTime.new(2016,01)
-      elsif string == "October"
-        DateTime.new(2016,10)
-      end
+      return DateTime.new(2016,10) if string == "October"
+      return DateTime.new(2016,11) if string == "November"
+      return DateTime.new(2016,12) if string == "December"
+      return DateTime.new(2017,01) if string == "January"
+      return DateTime.new(2017,01) if string == "February"
+      return DateTime.new(2017,01) if string == "March"
+      return DateTime.new(2017,01) if string == "April"
     end
 end
