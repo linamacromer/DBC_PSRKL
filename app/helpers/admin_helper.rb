f7314e09b8b141838c8beaa48980c1153a8c29bd
apprentice <apprentice@apprentice-6.local>
2016-10-27 12:01:43 -0700

    Add login/logout to Admin helper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,14 @@
 module AdminHelper
+
+  def login()
+    session[:logged_in] = true
+  end
+
+  def logged_in?
+    session[:logged_in]
+  end
+
+  def logout
+    session[:logged_in] = false
+  end
 end
