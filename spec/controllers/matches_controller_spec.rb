bc404f5828bf9569587222551b81ad20bb182f24
Lina Cromer <linamacromer@gmail.com>
2016-10-28 15:06:54 -0700

    Update matches_controller_spec.rb
    Add admin login before test

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe MatchesController, type: :controller do
 
+  before(:each) do
+    @request.session[:logged_in] = true
+  end
+  
   describe 'matches#index' do
     it 'when admin logged in the admin matches page loads' do
       expect(get :index).to render_template :index
