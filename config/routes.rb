f33e5e04c7ff0dad4bfb02f9bd676b74db8675d4
Jeffrey Mendiola & bradley inniss <pair+jwmendio.ratley@devbootcamp.com>
2016-10-28 16:15:11 -0700

    Update secret key

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ DBCPsrkl::Application.routes.draw do
     get "/login", to: "authenticate#login_form"
     post "/login", to: "authenticate#login"
     get "/logout", to: "authenticate#logout"
+    get "/matches/:id/results", to: "admin#edit"
     resources :competitors, only: [:new, :create, :index]
     resources :matches
   end
