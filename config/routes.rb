fbf86df383e92d57d591b0915a965ff7a64b7a8f
Paul Liu <paul.pliu87@gmail.com>
2016-10-28 16:24:02 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ DBCPsrkl::Application.routes.draw do
     get "/login", to: "authenticate#login_form"
     post "/login", to: "authenticate#login"
     get "/logout", to: "authenticate#logout"
-    get "/matches/:id/results", to: "admin#edit"
     resources :competitors, only: [:new, :create, :index]
     resources :matches
   end
