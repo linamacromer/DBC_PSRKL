2559df66281234aa3f41a08f72e9614bb7f18f78
apprentice <apprentice@apprentice-10.local>
2016-10-28 17:32:47 -0700

    Doesn;t work

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -3,13 +3,13 @@ module RoundsHelper
     return false if Round.count > 0
    # Return an array of the top 8 competitors
     top_competitors = Competitor.all_standings[0..7]
-    round1 = generate_round(top_competitors, Round.create(title: "Quarterfinals"))
+    round1 = RoundsController.generate_round(top_competitors, Round.create(title: "Quarterfinals"))
 
    # Return an array of the top 4 competitors
-    round2 = next_round(round1, "Semifinals")
+    round2 = RoundsController.next_round(round1, "Semifinals")
 
   # Return an array of the top 2 competitors
-    round3 = next_round(round2, "Finals")
+    round3 = RoundsController.next_round(round2, "Finals")
     true
   end
 
