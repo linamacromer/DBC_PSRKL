2446e6d27f7c2e2ca9263f74a061ec38ac0daeab
Jeffrey Mendiola & bradley inniss <pair+jwmendio.ratley@devbootcamp.com>
2016-10-28 17:57:35 -0700

    WIP: need to fix edit feature

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -8,4 +8,16 @@ class MatchesController < AdminController
   def edit
     @match = Match.find(params[:id])
   end
+
+  def update
+    @match = Match.find(params[:id])
+    @match.winner
+    p @match.winner
+    p params
+    redirect_to @match
+  end
+
+  def show
+    @match = Match.find(params[:id])
+  end
 end
